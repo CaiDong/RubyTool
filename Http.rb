f2eabfa0da177e34976af9f867f272cebefe9576
CaiDong <545965765@qq.com>
2015-04-17 15:26:02 +0800

    add

--- Http.rb
+++ Http.rb
@@ -30,6 +30,25 @@ def getJsonBody(appId,templateId,to,param)
 	return body
 end
 
+def TemplateSMS(sid,token,time,appId,templateId,to,param)
+	sig=getSig(sid,token,time)
+	auth=getAuth(sid,time)
+	uri=URI("https://api.ucpaas.com/2014-06-30/Accounts/#{sid}/Messages/templateSMS?sig=#{sig}")
+
+	https=Net::HTTP.new(uri.host,uri.port)
+	https.use_ssl=true
+
+	req=Net::HTTP::Post.new(uri)
+	req['Accept']='application/json'
+	req['Content-Type']='application/json;charset=utf-8'
+	req['Authorization']=auth
+	req.body=getJsonBody(appId,templateId,to,param)	
+	
+	response=https.request req 
+	puts "#{response.code}\n#{response.code}\n#{response.body}"
+
+end
+
 sid="409ab4c20241243879e4b8dffb17fe9e"
 token="7481ebd80d7888ca2963506087d4a0df"
 time=Time.now.strftime("%Y%m%d%H%M%S")
@@ -38,6 +57,7 @@ templateId="5314"
 to="13544516576"
 param="1234"
 
-getSig(sid,token,time)
-getAuth(sid,time)
-getJsonBody(appId,templateId,to,param)
+TemplateSMS(sid,token,time,appId,templateId,to,param)
+#getSig(sid,token,time)
+#getAuth(sid,time)
+#getJsonBody(appId,templateId,to,param)
