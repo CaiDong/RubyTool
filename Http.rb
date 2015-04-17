98aca80f0a9fe5fb5086cf0a88bc6273666720bf
CaiDong <545965765@qq.com>
2015-04-17 06:18:06 +0000

    add

--- Http.rb
+++ Http.rb
@@ -2,6 +2,7 @@ require 'net/https'
 require 'net/http'
 require 'digest'
 require 'base64'
+require 'json'
 
 def getSig(sid,token,timestamp)
 	
@@ -17,13 +18,26 @@ def getAuth(sid,timestamp)
 	return auth
 end
 
-def getJsonBody(appId,)
-	
+def getJsonBody(appId,templateId,to,param)
+	json={
+		"appId" => appId,
+		"templateId" => templateId,
+		"to" => to,
+		"param" => param
+	}.to_json
+	body ="{\"templateSMS\":#{json}}"
+	puts body
+	return body
 end
 
 sid="409ab4c20241243879e4b8dffb17fe9e"
 token="7481ebd80d7888ca2963506087d4a0df"
 time=Time.now.strftime("%Y%m%d%H%M%S")
+appId="d9d2f519037345f280c0f23a92ff01d1"
+templateId="5314"
+to="13544516576"
+param="1234"
 
 getSig(sid,token,time)
 getAuth(sid,time)
+getJsonBody(appId,templateId,to,param)
